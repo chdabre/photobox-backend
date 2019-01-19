abf890c9192ca7fd2e1186b2cb53344f3dc32faf
Dario Breitenstein <online@imakethings.ch>
2019-01-19 23:48:36 +0100

    poweroff instead of shutdown

--- photobox-backend.py
+++ photobox-backend.py
@@ -161,7 +161,7 @@ GPIO.add_event_detect(take_photo_pin, GPIO.FALLING, callback=button_callback, bo
 shutdown_pin = 8
 GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def shutdown_callback(channel):
-    print(subprocess.call(['sudo shutdown'], shell=True))
+    print(subprocess.call(['sudo poweroff'], shell=True))
 
 GPIO.add_event_detect(shutdown_pin, GPIO.FALLING, callback=shutdown_callback, bouncetime=1000)
 
